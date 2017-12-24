9b88282ad3fc9a0a2daabbf81e9436c756afce08
xiaoyu zhou <xiaoyuz2011@gmail.com>
2017-12-24 17:04:03 -0500

    add execution timer to main

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+var startTime = performance.now();
+
+// Implement your function here
+
+var endTime = performance.now();
+
+console.log("Main took " + (startTime - endTime) + " milliseconds to finish.")
