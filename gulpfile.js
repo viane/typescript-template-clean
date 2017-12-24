484c883bfef0781e4dc9736309c6a912fe264d89
xiaoyu zhou <xiaoyuz2011@gmail.com>
2017-12-24 01:57:12 -0500

    init

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var browserify = require('gulp-browserify');
+var merge = require('merge2');
+var tsProject = ts.createProject({declaration: true});
+var gulpSequence = require('gulp-sequence')
+
+gulp.task('scripts', function() {
+  var tsResult = gulp.src('src/**/*.ts').pipe(tsProject());
+  return merge([
+    // Merge the two output streams, so this task is finished when the IO of both operations is done.
+    tsResult.dts.pipe(gulp.dest('built/definitions')),
+    tsResult.js.pipe(gulp.dest('built/js'))
+  ]);
+});
+
+gulp.task('complie-main', () => {
+  gulp.src(['built/js/main.js']).pipe(browserify({insertGlobals: true})).pipe(gulp.dest('./built/js/'))
+})
+
+gulp.task('watch', function() {
+  gulpSequence('scripts', 'complie-main')(function(err) {
+    if (err)
+      console.log(err)
+  });
+  gulp.watch('src/**/*.ts', (event) => {
+    gulpSequence('scripts', 'complie-main')(function(err) {
+      if (err)
+        console.log(err)
+    })
+  })
+});
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Typescript practice
+
+Main focus on OOP and strong typed javascript implementation.
+
+```sh
+# Auto compile .ts to .js and start watching
+
+gulp watch
+
+```
