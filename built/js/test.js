5a2d21c8c02b4df1e57f727624b48aa0196f4fc8
xiaoyu zhou <xiaoyuz2011@gmail.com>
2017-12-30 17:52:59 -0500

    remove built dir

--- built/js/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var chai_1 = require("chai");
-var main_1 = require("./main");
-describe('Test hello world function', function () {
-    it('Should return "Hello World"', function () {
-        chai_1.expect(main_1.helloWorld()).to.equal("Hello World");
-    });
-});
