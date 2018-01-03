da7f5905b70d105ea3948a4dc45b4b50ee116700
xiaoyu zhou <xiaoyuz2011@gmail.com>
2018-01-03 00:26:00 -0500

    refactor/add
    +/- Refactor template main implantation into single block
    +    Add default export for test
    +    Default test for main function

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,35 @@
+/** Import Modules */
 import now = require("performance-now")
 
-var startTime = now()
-
 // Implement your function here
-const sleep = (miliseconds)=> {
-   var currentTime = new Date().getTime();
 
-   while (currentTime + miliseconds >= new Date().getTime()) {
-   }
+const main = (executionTime): void => {
+  let startTime: number = now(), endTime: number
+
+  const sleep = (miliseconds): void => {
+    const currentTime: number = new Date().getTime()
+    const holdUntil: number = currentTime + miliseconds
+    while (holdUntil >= new Date().getTime()) {
+    }
+  }
+
+  sleep(executionTime)
+
+  endTime = now()
+
+  console.log("\nTask took " + (endTime - startTime).toFixed(1) + " milliseconds to finish.\n")
 }
-sleep(827);
-// End of implementation
 
-var endTime = now()
+main(800)
 
-console.log("\nTask took " + (endTime - startTime).toFixed(1) + " milliseconds to finish.\n")
-
-
-export function helloWorld(){
+// Single function export
+export function helloWorld(): string {
   return "Hello World"
 }
+
+// Export function set
+export default {
+  defaultTest: { run: main, executionTime: 800 }
+}
+
+// End of implementation
--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,18 @@
-import {expect} from 'chai'
-import {helloWorld} from './main'
+import { expect } from 'chai'
+import now = require("performance-now")
+import { helloWorld } from './main'
+import exportFunctionSet from './main'
 
-describe('Test hello world function',()=>{
-  it('Should return "Hello World"',()=>{
+describe('Test hello world function', (): void => {
+  it('Should return "Hello World"', (): void => {
     expect(helloWorld()).to.equal("Hello World")
+  })
+
+  it('Should held 800ms to finish', (): void => {
+    const startTime: number = now()
+    const executionTime: number = exportFunctionSet.defaultTest.executionTime
+    exportFunctionSet.defaultTest.run(executionTime)
+    const endTime: number = now()
+    expect(endTime - startTime).to.greaterThan(800)
   })
 })
